584d979e623e79b8d5eb6f4a5abe6684e1647b12
AnatoliyNA <inlitebox@gmail.com>
2025-08-02 18:10:10 -0700

    Эксперементы с функциями

---
+++ sandbox/src/main/java/Geometry.java
@@ -0,0 +1,26 @@
+public class Geometry {
+    public static void main(String[] args) {
+        printSquareArea(7.0);
+        printSquareArea(5.0);
+        printSquareArea(3.0);
+
+        printRectangleAria(3.0, 5.0);
+        printRectangleAria(7.0, 9.0);
+    }
+
+    private static void printRectangleAria(double a, double b) {
+        System.out.println("Площадь примоугольника со сторонами " + a + " и " + b + " = " + rectangleArea(a, b));
+    }
+
+    private static double rectangleArea(double a, double b) {
+        return  a * b;
+    }
+
+    static void printSquareArea(double side) {
+        System.out.println("Площадь квадрата со стороной " + side + " = " + squareArea(side));
+    }
+
+    private static double squareArea(double side) {
+        return side * side;
+    }
+}
