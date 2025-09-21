e11e88d1a83c622b705cfb2193b80d369a29692c
AnatoliyNA <inlitebox@gmail.com>
2025-09-21 02:36:26 -0700

    Убран тест удаления группы из контакта, перенесен в тесты модификации

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactRemovalTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactRemovalTests.java
@@ -16,7 +16,7 @@ public class ContactRemovalTests extends TestBase{
     @Test
     public void canRemoveContact() {
         if (app.hbm().getContactCount() == 0) {
-            app.hbm().createContact(new ContactData("", "firstName", "middleName", "lastName", "nickName", "title", "company", "address", "home", "mobile", "work", "fax", "email", "email2", "email3", "homepage", ""));
+            app.hbm().createContact(new ContactData("", "firstName", "middleName", "lastName", "nickName", "title", "company", "address", "home", "mobile", "work", "fax", "email", "email2", "email3", "homepage", "", ""));
         }
         var oldContacts = app.hbm().getContactList();
         var rnd = new Random();
@@ -31,39 +31,11 @@ public class ContactRemovalTests extends TestBase{
     @Test
     public void canRemoveAllContact() {
         if (app.hbm().getContactCount() == 0) {
-            app.hbm().createContact(new ContactData("", "firstName", "middleName", "lastName", "nickName", "title", "company", "address", "home", "mobile", "work", "fax", "email", "email2", "email3", "homepage", ""));
+            app.hbm().createContact(new ContactData("", "firstName", "middleName", "lastName", "nickName", "title", "company", "address", "home", "mobile", "work", "fax", "email", "email2", "email3", "homepage", "", ""));
         }
         app.contacts().removeAllContact();
         var newList = app.hbm().getContactList();
         var expectedList = Collections.emptyList();
         Assertions.assertEquals(expectedList, newList);
-    }
-
-    @Test
-    public void canCreateContactInGroup() {
-        var contact = new ContactData()
-                .withMiddleName(CommonFunctions.randomString(10))
-                .withFirstName(CommonFunctions.randomString(10))
-                .withLastName(CommonFunctions.randomString(10))
-                .withAddress(CommonFunctions.randomString(10));
-        if (app.hbm().getGroupCount() == 0) {
-            app.hbm().createGroup(new GroupData("", "group name",
-                    "group header", "group footer"));
-        }
-        Comparator<ContactData> compareById = (o1, o2) -> {
-            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
-        };
-        var group = app.hbm().getGroupList().get(0);
-        var oldRelated = app.hbm().getContactsInGroup(group);
-        app.contacts().createContact(contact, group);
-        var newRelated = app.hbm().getContactsInGroup(group);
-        oldRelated.sort(compareById);
-        newRelated.sort(compareById);
-        var newContact = newRelated.get(newRelated.size() - 1);
-        app.contacts().removeContactFromGroup(newContact, group);
-        ArrayList<ContactData> expectedRelated = new ArrayList<>(oldRelated);
-        expectedRelated.remove(newContact);
-        expectedRelated.sort(compareById);
-        Assertions.assertEquals(expectedRelated, oldRelated);
     }
 }
