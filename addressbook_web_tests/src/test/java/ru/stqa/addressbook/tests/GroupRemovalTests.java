3cd7643d1cc18a3c6a65823a56fb9d2c232c02e2
AnatoliyNA <inlitebox@gmail.com>
2025-09-25 20:39:55 -0700

    правки по GroupRemovalTests

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupRemovalTests.java
@@ -1,9 +1,13 @@
 package ru.stqa.addressbook.tests;
 
+import io.qameta.allure.Allure;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
 import ru.stqa.addressbook.model.GroupData;
 import org.junit.jupiter.api.Test;
 
+import javax.annotation.processing.Generated;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,7 +16,8 @@ import java.util.Random;
 public class GroupRemovalTests extends TestBase{
 
     @Test
-    public void canRemoveGrope() {
+    @Description("Тест удаляет группу")
+    public void canRemoveGroup() {
         if (app.hbm().getGroupCount() == 0) {
             app.hbm().createGroup(new GroupData("", "name", "name Heder", "name Footer"));
         }
@@ -23,10 +28,13 @@ public class GroupRemovalTests extends TestBase{
         var newGroups = app.hbm().getGroupList();
         var expectedList = new ArrayList<>(oldGroups);
         expectedList.remove(index);
-        Assertions.assertEquals(newGroups, expectedList);
+        Allure.step("Validating result", step -> {
+                Assertions.assertEquals(newGroups, expectedList);
+        });
     }
 
     @Test
+    @Generated("Тест удаляет все группы")
     public void canRemoveAllGropes() {
         if (app.hbm().getGroupCount() == 0) {
             app.hbm().createGroup(new GroupData("", "name", "name Heder", "name Footer"));
